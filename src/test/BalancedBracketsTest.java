e9bcc947a0585d27d21e8006d64e9930cc652460
Sarah Poulton <poultonse@gmail.com>
2021-02-01 19:33:30 -0600

    idk?

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,40 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+//    @Test
+//    public void emptyTest() {
+//        assertEquals(true, true);
+//    }
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void noBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyOneBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    pub
 
 
 }
